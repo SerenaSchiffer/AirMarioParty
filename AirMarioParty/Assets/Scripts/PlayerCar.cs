4755fe75f9f7a6633d66778a37d3e7a58a41b05b
patsimd <Daxter733@hotmail.com>
2016-12-08 01:46:50 -0500

    Controler l'auto avec le cell
    Jai fait une interface pour le cell pour la conduite de chars (c'est
    juste une fleche de chaque cote). Quand on pese sur play pis on connecte
    un cell, le char avance pis on peut le controler avec les fleches

--- AirMarioParty/Assets/Scripts/PlayerCar.cs
+++ AirMarioParty/Assets/Scripts/PlayerCar.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
 
 public class PlayerCar : MonoBehaviour {
     public static int goalCornets;
@@ -7,7 +9,9 @@ public class PlayerCar : MonoBehaviour {
     public int playerNumber;
     public int nombreCornets;
     public float speed = 3f;
+    public float turnAxis = 0;
     public float rotationSpeed = 3f;
+    public bool gameStarted = false;
 
     private Rigidbody2D myRB2D;
 	// Use this for initialization
@@ -15,15 +19,80 @@ public class PlayerCar : MonoBehaviour {
         nombreCornets = 0;
         playerNumber = 1;
         myRB2D = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        myRB2D.velocity = transform.up * speed;
+        AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onConnect += OnConnect;
+        AirConsole.instance.onDisconnect += OnDisconnect;
+    }
 
-        if(Input.GetAxis("Horizontal") != 0)
+    void OnMessage(int device_id, JToken data)
+    {
+        int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+        if (active_player != -1)
         {
-            transform.Rotate(new Vector3(0, 0, -Input.GetAxis("Horizontal") * rotationSpeed));
+            if (active_player == 0)
+            {
+                turnAxis = -(float)data["move"];
+            }
+            /*if (active_player == 1)
+            {
+                this.racketRight.velocity = Vector3.up * (float)data["move"];
+            }*/
+        }
+    }
+
+    void OnConnect(int device_id)
+    {
+        if (AirConsole.instance.GetActivePlayerDeviceIds.Count == 0)
+        {
+            if (AirConsole.instance.GetControllerDeviceIds().Count >= 2)
+            {
+                StartGame();
+            }
+           /* else
+            {
+                uiText.text = "NEED MORE PLAYERS";
+            }*/
+        }
+        StartGame();
+    }
+
+    void OnDisconnect(int device_id)
+    {
+        int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+        if (active_player != -1)
+        {
+            if (AirConsole.instance.GetControllerDeviceIds().Count >= 2)
+            {
+                StartGame();
+            }
+            else
+            {
+                gameStarted = false;
+                /*AirConsole.instance.SetActivePlayers(0);
+                ResetBall(false);
+                uiText.text = "PLAYER LEFT - NEED MORE PLAYERS";*/
+            }
+        }
+    }
+
+    void StartGame()
+    {
+        AirConsole.instance.SetActivePlayers(AirConsole.instance.GetControllerDeviceIds().Count);
+        gameStarted = true;
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+        if (gameStarted)
+            myRB2D.velocity = transform.up * speed;
+        else
+            myRB2D.velocity = transform.up *  0f;
+
+        if (turnAxis != 0)
+        {
+            transform.Rotate(new Vector3(0, 0, -turnAxis * rotationSpeed));
         }
 	}
 
